0a965da94d536696fd97a5d5190d9866fbbbdd46
Justin Sanchez <justinlsanchez12@gmail.com>
2023-07-31 00:50:54 -0700

    updated connection now server connects with mongodb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,8 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/developersApplications', {
+connect('mongodb://127.0.0.1:27017/developersApplications', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Get the default connection
-const db = connection; 
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => console.log('Connected to MongoDB'));
-
-module.exports = db;
+module.exports = connection;
